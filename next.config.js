403115d7e8ea16bc099faad9f972e23246410a8f
NCgrmny <nextchaptergermany@gmail.com>
2026-01-16 12:24:53 +0100

    Fix: Duplicate Sentry config in next.config.js causing build failure

--- next.config.js
+++ next.config.js
@@ -12,13 +12,13 @@ const nextConfig = {
   },
   swcMinify: true, // Faster minification
   productionBrowserSourceMaps: false, // Save memory/CPU during build
-  
+
   // Compression
   compress: true,
-  
+
   // PoweredBy header removal (security)
   poweredByHeader: false,
-  
+
   // React Strict Mode (development only to avoid double renders)
   reactStrictMode: process.env.NODE_ENV === 'development',
 
@@ -74,78 +74,29 @@ const sentryWebpackPluginOptions = {
 
   // Suppresses source map uploading logs during build
   silent: true,
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  
-  // Only upload source maps in production
-  dryRun: process.env.NODE_ENV !== 'production' || !process.env.SENTRY_AUTH_TOKEN,
-  
-  // Hide source maps from Sentry
-  hideSourceMaps: true,
-  
-  // Don't widen error handling
-  widenClientFileUpload: false,
-  
-  // Don't transpile client SDK
-  transpileClientSDK: false,
-  
-  // Don't tunnel routes through Sentry
-  tunnelRoute: undefined,
-  
-  // Don't automatically hide source maps
-  hideSourceMaps: false,
-  
-  // Don't automatically disable Sentry during build
-  disableClientWebpackPlugin: false,
-  disableServerWebpackPlugin: false,
-  
-  // Don't automatically instrument server
-  automaticVercelMonitors: false,
-};
-
-// Export with Sentry configuration
-// Always use withSentryConfig - DSN can be set at runtime
-// This allows DSN to be set in Vercel after initial deployment
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(module.exports, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "saimor",
   project: "javascript-nextjs",
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+  // Only upload source maps in production
+  dryRun: process.env.NODE_ENV !== 'production' || !process.env.SENTRY_AUTH_TOKEN,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Hide source maps from Sentry
+  hideSourceMaps: true,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  // Upload a larger set of source maps for prettier stack traces
   widenClientFileUpload: true,
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: "/monitoring",
 
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
+  // Don't automatically instrument server (manual setup preferred for stability)
+  automaticVercelMonitors: true,
 
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
+  // Tree-shaking options for reducing bundle size
+  treeshake: {
+    removeDebugLogging: true,
   },
-});
+};
+
+// Export with Sentry configuration
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
