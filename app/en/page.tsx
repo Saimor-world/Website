78f8aba5b954984e6e99f3231dc598652cb18fd6
NCgrmny <nextchaptergermany@gmail.com>
2026-09-09 15:03:32 +0200

    place creator signal easter egg on English homepage

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -1,3 +1,4 @@
+import CreatorSignalEgg from '@/components/CreatorSignalEgg';
 import LivingForestWorld from '@/components/LivingForestWorld';
 
 export const metadata = {
@@ -6,5 +7,10 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <LivingForestWorld locale="en" />;
+  return (
+    <>
+      <LivingForestWorld locale="en" />
+      <CreatorSignalEgg locale="en" />
+    </>
+  );
 }
