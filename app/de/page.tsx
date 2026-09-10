d07a62893e1618af36727d5ea63270b5d96dd21b
NCgrmny <nextchaptergermany@gmail.com>
2026-09-10 16:47:26 +0200

    Homepage: switch to reliable YORI gate

--- app/de/page.tsx
+++ app/de/page.tsx
@@ -1,4 +1,4 @@
-import CreatorSignalEgg from '@/components/CreatorSignalEgg';
+import YoriGate from '@/components/YoriGate';
 import HomepageReframe from '@/components/HomepageReframe';
 import LivingForestWorld from '@/components/LivingForestWorld';
 
@@ -12,7 +12,7 @@ export default function Page() {
     <>
       <HomepageReframe />
       <LivingForestWorld locale="de" />
-      <CreatorSignalEgg locale="de" />
+      <YoriGate locale="de" />
     </>
   );
 }
